fad2b68c1510cb99790988d2370a4b48fab2c61e
Jazmin <benitezjazmin368@gmail.com>
2026-03-28 22:08:16 -0300

    ...

--- config/config.py
+++ config/config.py
@@ -3,19 +3,20 @@ import os
 
 load_dotenv()
 
-user= os.getenv('MYSQLUSER')
-password=os.getenv('MYSQLPASSWORD')
-host=os.getenv('MYSQLHOST')
-database=os.getenv('MYSQLDATABASE')
-port = os.getenv('MYSQLPORT', '3306')
+# Nombres exactos según tu lista de Railway
+user = os.getenv('MYSQL_USER')      
+password = os.getenv('MYSQL_PASSWORD') 
+host = os.getenv('MYSQL_HOST')      
+database = os.getenv('MYSQL_DB')    
+port = os.getenv('MYSQL_PORT', '3306') 
 
+# Construcción de la URI
 DATABASE_CONNECTION_URI = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
 
-MAX_CONTENT_LENGTH= 2*1024*1024 #2MB Máximo, va al handler de app
+MAX_CONTENT_LENGTH = 2 * 1024 * 1024 
 
 SQLALCHEMY_DATABASE_URI = DATABASE_CONNECTION_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = os.getenv("SECRET_KEY")
 MP_ACCESS_TOKEN = os.getenv("MP_ACCESS_TOKEN")
-
 BASE_URL = os.getenv("BASE_URL")
