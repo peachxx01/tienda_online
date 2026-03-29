5c2942c1d2e7020d214ecba0329a5e1c759a2278
Jazmin <benitezjazmin368@gmail.com>
2026-03-28 23:36:20 -0300

    ....

--- config/config.py
+++ config/config.py
@@ -1,24 +1,16 @@
 import os
 
-# Forzamos la obtención de las variables
-user = os.environ.get('MYSQLUSER')      
-password = os.environ.get('MYSQLPASSWORD') 
-host = os.environ.get('MYSQLHOST')      
-database = os.environ.get('MYSQLDATABASE')    
-port_db = os.environ.get('MYSQLPORT', '3306') 
-
-# DEBUG: Esto nos dirá en el log de Railway si las variables están llegando
-if not host:
-    print("--- ERROR: NO SE ENCONTRÓ MYSQLHOST EN LAS VARIABLES ---")
-else:
-    print(f"--- CONECTANDO A HOST: {host} ---")
+user = os.environ.get('MYSQLUSER')
+password = os.environ.get('MYSQLPASSWORD')
+host = os.environ.get('MYSQLHOST')
+database = os.environ.get('MYSQLDATABASE')
+port_db = os.environ.get('MYSQLPORT')
 
 # Construcción de la URI
 SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{user}:{password}@{host}:{port_db}/{database}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-MAX_CONTENT_LENGTH = 2 * 1024 * 1024 
-
-SECRET_KEY = os.environ.get("SECRETKEY") 
+MAX_CONTENT_LENGTH = 2 * 1024 * 1024
+SECRET_KEY = os.environ.get("SECRETKEY")
 MP_ACCESS_TOKEN = os.environ.get("MP_ACCESS_TOKEN")
 BASE_URL = os.environ.get("BASE_URL")
